3ef50defbfd57314173433f8b239e86db6346d34
Arnaud RIGAUT <arnaud.rigaut@viacesi.fr>
2018-12-05 15:27:19 +0100

    clean

--- ProjetTest/ProjetTest/Program.cs
+++ ProjetTest/ProjetTest/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //hello test
         }
     }
 }
