1340d86531b30151e77183addf31ec8d006f27bc
Arnaud RIGAUT <arnaud.rigaut@viacesi.fr>
2018-12-05 15:11:37 +0100

    Add solution

---
+++ ProjetTest/ProjetTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
