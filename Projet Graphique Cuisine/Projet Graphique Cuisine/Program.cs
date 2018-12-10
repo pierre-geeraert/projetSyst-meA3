f944d6dcf258d3a651b122357376f5d2baa20568
MaxenceArnol <maxence.arnol@viacesi.fr>
2018-12-10 17:03:56 +0100

    Projet Graphique Cuisine

---
+++ Projet Graphique Cuisine/Projet Graphique Cuisine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projet_Graphique_Cuisine
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new CuisineVue())
+                game.Run();
+        }
+    }
+}
