bb6b3c9a5603c31e11c5830d5efd3326b4845043
tapafe <arnaud.rigaut@viacesi.fr>
2018-12-10 16:20:44 +0100

    ajout ipcClient

--- ProjetTest/Salle2/model/ipcClient.cs
+++ ProjetTest/Salle2/model/ipcClient.cs
@@ -8,37 +8,7 @@ namespace Salle2.model
 {
     public class SocketClient
     {
-<<<<<<< HEAD
-        // Client socket.  
-        public Socket workSocket = null;
-        // Size of receive buffer.  
-        public const int BufferSize = 256;
-        // Receive buffer.  
-        public byte[] buffer = new byte[BufferSize];
-        // Received data string.  
-        public StringBuilder sb = new StringBuilder();
-    }
-
-    public class AsynchronousClient
-    {
-        // The port number for the remote device.  
-        private const int port = 11000;
-
-        // ManualResetEvent instances signal completion.  
-        private static ManualResetEvent connectDone =
-            new ManualResetEvent(false);
-        private static ManualResetEvent sendDone =
-            new ManualResetEvent(false);
-        private static ManualResetEvent receiveDone =
-            new ManualResetEvent(false);
-
-        // The response from the remote device.  
-        private static String response = String.Empty;
-
-        public static void StartClient(string commande)
-=======
         public static void StartClient()
->>>>>>> master
         {
             byte[] bytes = new byte[1024];
 
@@ -56,37 +26,11 @@ namespace Salle2.model
                 Socket sender = new Socket(ipAddress.AddressFamily,
                     SocketType.Stream, ProtocolType.Tcp);
 
-<<<<<<< HEAD
-                // Connect to the remote endpoint.  
-                client.BeginConnect(remoteEP,
-                    new AsyncCallback(ConnectCallback), client);
-                connectDone.WaitOne();
-
-                // Send test data to the remote device.  
-                Send(client, commande);
-                sendDone.WaitOne();
-
-                // Receive the response from the remote device.  
-                Receive(client);
-                receiveDone.WaitOne();
-
-                // Release the socket.  
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-=======
                 // Connect the socket to the remote endpoint. Catch any errors.    
                 try
                 {
                     // Connect to Remote EndPoint  
                     sender.Connect(remoteEP);
->>>>>>> master
 
                     Console.WriteLine("Socket connected to {0}",
                         sender.RemoteEndPoint.ToString());
@@ -94,16 +38,6 @@ namespace Salle2.model
                     // Encode the data string into a byte array.    
                     byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
 
-<<<<<<< HEAD
-                // Signal that the connection has been made.  
-                connectDone.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-=======
                     // Send the data through the socket.    
                     int bytesSent = sender.Send(msg);
 
@@ -111,7 +45,6 @@ namespace Salle2.model
                     int bytesRec = sender.Receive(bytes);
                     Console.WriteLine("Echoed test = {0}",
                         Encoding.ASCII.GetString(bytes, 0, bytesRec));
->>>>>>> master
 
                     // Release the socket.    
                     sender.Shutdown(SocketShutdown.Both);
@@ -120,15 +53,7 @@ namespace Salle2.model
                 }
                 catch (ArgumentNullException ane)
                 {
-<<<<<<< HEAD
-                    // There might be more data, so store the data received so far.  
-                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
-
-                    // Get the rest of the data.  
-                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
-=======
                     Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
->>>>>>> master
                 }
                 catch (SocketException se)
                 {
@@ -138,36 +63,7 @@ namespace Salle2.model
                 {
                     Console.WriteLine("Unexpected exception : {0}", e.ToString());
                 }
-<<<<<<< HEAD
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
 
-        private static void Send(Socket client, String data)
-        {
-            // Convert the string data to byte data using ASCII encoding.  
-            byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-            // Begin sending the data to the remote device.  
-            client.BeginSend(byteData, 0, byteData.Length, 0,
-                new AsyncCallback(SendCallback), client);
-        }
-
-        private static void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.  
-                Socket client = (Socket)ar.AsyncState;
-
-                // Signal that all bytes have been sent.  
-                sendDone.Set();
-=======
-
->>>>>>> master
             }
             catch (Exception e)
             {
