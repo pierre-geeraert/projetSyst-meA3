52a489bec09660ad0663c098c15e63063d0fbfe8
MaxenceArnol <maxence.arnol@viacesi.fr>
2018-12-10 17:33:16 +0100

    Projet Graphique Salle

---
+++ Projet Graphique Salle/Projet Graphique Salle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projet_Graphique_Salle
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new SalleVue())
+                game.Run();
+        }
+    }
+}
